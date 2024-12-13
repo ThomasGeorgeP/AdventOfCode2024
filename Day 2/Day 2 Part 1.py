0da577d46d17a6cf7c93dc936238f77b80dcc2a1
Thomas George Panicker <gpthomas101@gmail.com>
2024-12-13 21:07:59 +0530

    Day 2 Formatted Properly and Day 3 added

---
+++ Day 2/Day 2 Part 1.py
@@ -0,0 +1,19 @@
+with open('data.txt','r') as f:
+    data=f.read()
+
+data=data.split('\n')
+data=[[int(j) for j in i.split()] for i in data]
+print(data)
+safe=0
+for k in data:
+
+    i=k.copy()
+    
+    if i==sorted(i) or i==sorted(i,reverse=True):
+        for j in range(len(i)-1):
+            if abs(i[j]-i[j+1])>3 or abs(i[j]-i[j+1])<1:
+                break
+        else:
+            safe+=1
+            
+print(safe)
---
+++ Day 2/Day 2 Part 2.py
@@ -0,0 +1,18 @@
+with open('data.txt','r') as f:
+    data=f.read()
+    data=data.split('\n')
+    data=[[int(j) for j in i.split()] for i in data]
+
+    safe=0
+    for k in data:
+        for ind in range(len(k)):
+            i=k.copy()
+            del i[ind]
+            if i==sorted(i) or i==sorted(i,reverse=True):
+                for j in range(len(i)-1):
+                    if abs(i[j]-i[j+1])>3 or abs(i[j]-i[j+1])<1:
+                        break
+                else:
+                    safe+=1
+                    break
+    print(safe)
